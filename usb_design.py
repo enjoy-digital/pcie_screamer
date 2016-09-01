1dffdff6975cfa7c2a3e72171108bce1fe64e5e7
Florent Kermarrec <florent@enjoy-digital.fr>
2016-09-01 12:08:56 +0200

    usb_design: simplify, remove lm32/ddr and add timing constraint on 100Mhz input usb clk

--- usb_design.py
+++ usb_design.py
@@ -3,114 +3,34 @@ import argparse
 import os
 
 from litex.gen import *
+from litex.gen.genlib.io import CRG
 from litex.gen.genlib.resetsync import AsyncResetSynchronizer
 from litex.gen.fhdl.specials import Keep
 
 import platform as pcie_injector
 
-from litex.soc.integration.soc_core import mem_decoder
-from litex.soc.integration.soc_sdram import *
+from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 from litex.soc.interconnect import stream
-
-from litedram.modules import MT41K256M16
-from litedram.phy import a7ddrphy
+from litex.soc.cores.uart.bridge import UARTWishboneBridge
 
 from ft245 import phy_description, FT245PHYSynchronous
 
 
-class _CRG(Module):
+class BaseSoC(SoCCore):
     def __init__(self, platform):
-        self.clock_domains.cd_sys = ClockDomain()
-        self.clock_domains.cd_sys4x = ClockDomain(reset_less=True)
-        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
-        self.clock_domains.cd_clk200 = ClockDomain()
-        self.clock_domains.cd_clk100 = ClockDomain()
-
-        clk100 = platform.request("usb_fifo_clock")
-        rst = Signal(reset=1) # FIXME
-
-        pll_locked = Signal()
-        pll_fb = Signal()
-        self.pll_sys = Signal()
-        pll_sys4x = Signal()
-        pll_sys4x_dqs = Signal()
-        pll_clk200 = Signal()
-        self.specials += [
-            Instance("PLLE2_BASE",
-                     p_STARTUP_WAIT="FALSE", o_LOCKED=pll_locked,
-
-                     # VCO @ 1600 MHz
-                     p_REF_JITTER1=0.01, p_CLKIN1_PERIOD=10.0,
-                     p_CLKFBOUT_MULT=16, p_DIVCLK_DIVIDE=1,
-                     i_CLKIN1=clk100, i_CLKFBIN=pll_fb, o_CLKFBOUT=pll_fb,
-
-                     # 100 MHz
-                     p_CLKOUT0_DIVIDE=16, p_CLKOUT0_PHASE=0.0,
-                     o_CLKOUT0=self.pll_sys,
-
-                     # 400 MHz
-                     p_CLKOUT1_DIVIDE=4, p_CLKOUT1_PHASE=0.0,
-                     o_CLKOUT1=pll_sys4x,
-
-                     # 400 MHz dqs
-                     p_CLKOUT2_DIVIDE=4, p_CLKOUT2_PHASE=90.0,
-                     o_CLKOUT2=pll_sys4x_dqs,
-
-                     # 200 MHz
-                     p_CLKOUT3_DIVIDE=8, p_CLKOUT3_PHASE=0.0,
-                     o_CLKOUT3=pll_clk200,
-
-                     # 400MHz
-                     p_CLKOUT4_DIVIDE=4, p_CLKOUT4_PHASE=0.0,
-                     #o_CLKOUT4=
-            ),
-            Instance("BUFG", i_I=self.pll_sys, o_O=self.cd_sys.clk),
-            Instance("BUFG", i_I=pll_sys4x, o_O=self.cd_sys4x.clk),
-            Instance("BUFG", i_I=pll_sys4x_dqs, o_O=self.cd_sys4x_dqs.clk),
-            Instance("BUFG", i_I=pll_clk200, o_O=self.cd_clk200.clk),
-            Instance("BUFG", i_I=clk100, o_O=self.cd_clk100.clk),
-            AsyncResetSynchronizer(self.cd_sys, ~pll_locked | ~rst),
-            AsyncResetSynchronizer(self.cd_clk200, ~pll_locked | rst),
-            AsyncResetSynchronizer(self.cd_clk100, ~pll_locked | rst)
-        ]
-
-        reset_counter = Signal(4, reset=15)
-        ic_reset = Signal(reset=1)
-        self.sync.clk200 += \
-            If(reset_counter != 0,
-                reset_counter.eq(reset_counter - 1)
-            ).Else(
-                ic_reset.eq(0)
-            )
-        self.specials += Instance("IDELAYCTRL", i_REFCLK=ClockSignal("clk200"), i_RST=ic_reset)
-
-
-class BaseSoC(SoCSDRAM):
-    csr_map = {
-        "ddrphy": 17,
-        "dna":    18,
-        "xadc":   19
-    }
-    csr_map.update(SoCSDRAM.csr_map)
-
-    def __init__(self, platform, **kwargs):
         clk_freq = 100*1000000
-        SoCSDRAM.__init__(self, platform, clk_freq,
-            integrated_rom_size=0x8000,
-            integrated_sram_size=0x8000,
-            **kwargs)
-
-        self.submodules.crg = _CRG(platform)
-
-        # sdram
-        self.submodules.ddrphy = a7ddrphy.A7DDRPHY(platform.request("ddram"))
-        self.add_constant("A7DDRPHY_BITSLIP", 2)
-        self.add_constant("A7DDRPHY_DELAY", 8)
-        sdram_module = MT41K256M16(self.clk_freq, "1:4")
-        self.register_sdram(self.ddrphy,
-                            sdram_module.geom_settings,
-                            sdram_module.timing_settings)
+        SoCCore.__init__(self, platform, clk_freq,
+            cpu_type=None,
+            csr_data_width=32,
+            with_uart=False,
+            ident="USB loopback example design",
+            with_timer=False
+        )
+        self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"), clk_freq, baudrate=115200))
+        self.add_wb_master(self.cpu_or_bridge.wishbone)
+        clk100 = platform.request("usb_fifo_clock")
+        self.submodules.crg = CRG(clk100)
 
         # led blink
         counter = Signal(32)
@@ -123,13 +43,7 @@ class BaseSoC(SoCSDRAM):
         # usb
         self.comb += platform.request("usb_fifo_rst").eq(1)
         usb_pads = platform.request("usb_fifo")
-        self.comb += [
-            #usb_pads.oe_n.eq(1),
-            #usb_pads.rd_n.eq(1),
-            #usb_pads.wr_n.eq(1),
-            #usb_pads.siwua.eq(1),
-            usb_pads.be.eq(0xf)
-        ]
+        self.comb += usb_pads.be.eq(0xf)
 
         self.submodules.usb_phy = FT245PHYSynchronous(usb_pads, 100*1000000)
         self.submodules.usb_loopback_fifo = stream.SyncFIFO(phy_description(32), 8192)
@@ -138,15 +52,17 @@ class BaseSoC(SoCSDRAM):
             self.usb_loopback_fifo.source.connect(self.usb_phy.sink)
         ]
 
+        self.platform.add_period_constraint(clk100, 10.0)
+
 
 def main():
     parser = argparse.ArgumentParser(description="PCIe Injector LiteX SoC")
     builder_args(parser)
-    soc_sdram_args(parser)
+    soc_core_args(parser)
     args = parser.parse_args()
 
     platform = pcie_injector.Platform()
-    soc = BaseSoC(platform, **soc_sdram_argdict(args))
+    soc = BaseSoC(platform, **soc_core_argdict(args))
     builder = Builder(soc, output_dir="build")
     vns = builder.build()
 
