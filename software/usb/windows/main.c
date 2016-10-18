f1fb2ab357bc7fddfb2518d4cd2f4b6d7cd3d7c9
Florent Kermarrec <florent@enjoy-digital.fr>
2016-10-18 12:36:55 +0200

    usb: cleanup server code

--- software/usb/windows/main.c
+++ software/usb/windows/main.c
@@ -1,262 +1,229 @@
-#include <string.h>
-#include <errno.h>
 #include <stdio.h>
-#include <signal.h>
 #ifndef WIN32
-#include <netinet/in.h>
-# ifdef _XOPEN_SOURCE_EXTENDED
-#  include <arpa/inet.h>
-# endif
 #include <sys/socket.h>
 #endif
 
-#include <event2/bufferevent.h>
-#include <event2/buffer.h>
-#include <event2/listener.h>
-#include <event2/util.h>
 #include <event2/event.h>
 #include "FTD3XX.h"
 
-FT_HANDLE ftHandle;
+FT_HANDLE ft_handle;
 
-#define STRM_MAGIC 0x5aa55aa5
-struct ft_stream{
-  short port;
-  int fd;
-  unsigned char canal;
-  struct event_base *base;
-  struct sockaddr_in  client_addr;
+#define FT_STREAM_PREAMBLE 0x5aa55aa5
+#define FT_STREAM_HEADER_SIZE 12
+#define FT_STREAM_PORTS 256
+
+#define DEBUG
+
+struct ft_stream {
+    short udp_port;
+    unsigned char usb_port;
+    int fd;
+    struct event_base * base;
+    struct sockaddr_in client_addr;
 };
 
-struct ft_stream streams[256];
+struct ft_stream ft_streams[FT_STREAM_PORTS];
 
-
-
-void read_cb(int fd, short event, void *arg)
+void udp_callback(int fd, short event, void *arg)
 {
-  struct ft_stream *st = (struct ft_stream*)arg;
-  char                buf[1500];
-  int                 len;
-  int                 size = sizeof(struct sockaddr);
-  int header[3];
-  unsigned char *out;
-  int i;
+    struct ft_stream *s = (struct ft_stream*) arg;
 
-  memset(buf, 0, sizeof(buf));
-  len = recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&st->client_addr, &size);
+    char udp_rxbuf[1500];
+    int udp_rxlen;
+    int udp_rxsize;
 
-  if (len == -1) {
-    perror("recvfrom()");
-  } else if (len == 0) {
-    printf("Connection Closed\n");
-  } else {
-    printf("Read: len [%d] - content [%s]\n", len, buf);
-    //header[0] = ntohl(STRM_MAGIC);
-    //header[1] = ntohl(st->canal);
-    //header[2] = ntohl(len);
-    header[0] = STRM_MAGIC;
-    header[1] = st->canal;
-    header[2] = len;
-    out = malloc(len + 12);
-    memcpy(out, header, 12);
-    memcpy(out + 12, buf, len);
-    FT601_Write(ftHandle, out, len + 12);
-    printf("sending:\n");
-    for(i = 0; i < len + 12; i++)
-      printf("%02x ", out[i]);
-    printf("\n\n");
-    free(out);
-    /* Echo */
-    //sendto(fd, buf, len, 0, (struct sockaddr *)&client_addr, size);
-  }
+    int usb_txheader[FT_STREAM_HEADER_SIZE/4];
+    unsigned char usb_txbuf[1500 + FT_STREAM_HEADER_SIZE];
+
+    udp_rxsize = sizeof(struct sockaddr);
+    udp_rxlen = recvfrom(fd, udp_rxbuf, sizeof(udp_rxbuf), 0,
+      (struct sockaddr *)&s->client_addr, &udp_rxsize);
+
+    if (udp_rxlen == -1) {
+        perror("recvfrom()");
+    } else if (udp_rxlen > 0) {
+        usb_txheader[0] = FT_STREAM_PREAMBLE;
+        usb_txheader[1] = s->usb_port;
+        usb_txheader[2] = udp_rxlen;
+        memcpy(usb_txbuf, usb_txheader, FT_STREAM_HEADER_SIZE);
+        memcpy(usb_txbuf + FT_STREAM_HEADER_SIZE, udp_rxbuf, udp_rxlen);
+        FT601_Write(ft_handle, usb_txbuf, udp_rxlen + FT_STREAM_HEADER_SIZE);
+#ifdef DEBUG
+        int i;
+        printf("usb_tx: ");
+        for(i=0; i<udp_rxlen + FT_STREAM_HEADER_SIZE; i++)
+            printf("%02x", usb_txbuf[i]);
+        printf("\n");
+#endif
+    }
 }
 
-int new_canal(struct event_base *base, unsigned char canal, short port)
+int new_usb_udp_bridge(struct event_base *base, unsigned char usb_port,
+  short udp_port)
 {
-  struct sockaddr_in sin;
-  struct evconnlistener *listener;
-  struct event *ev;
-  int sock_fd;
-  char flag = 1;
-  struct ft_stream *st;
+    struct sockaddr_in socket_sin;
+    int socket_fd;
+    char socket_flag = 1;
 
-  st = &streams[canal];
-  memset(st, 0, sizeof(struct ft_stream));
-  st->port = port;
-  st->canal = canal;
-  st->base = base;
+    struct event *ev;
 
+    struct ft_stream *s;
 
-  memset(&sin, 0, sizeof(sin));
-  sin.sin_family = AF_INET;
-  sin.sin_port = htons(port);
+    s = &ft_streams[usb_port];
+    s->udp_port = udp_port;
+    s->usb_port = usb_port;
+    s->base = base;
 
-  if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("socket()");
-    return -1;
-  }
-  st->fd = sock_fd;
-  if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(char)) < 0) {
-    perror("setsockopt()");
-    return 1;
-  }
+    memset(&socket_sin, 0, sizeof(socket_sin));
+    socket_sin.sin_family = AF_INET;
+    socket_sin.sin_port = htons(udp_port);
 
-  if (bind(sock_fd, (struct sockaddr *)&sin, sizeof(struct sockaddr)) < 0) {
-    perror("bind()");
-    return -1;
-  } else {
-    printf("bind() success - [%s] [%u]\n", "local", port);
-  }
+    if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket()");
+        return -1;
+    }
+    s->fd = socket_fd;
+    if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR,
+      &socket_flag, sizeof(char)) < 0) {
+        perror("setsockopt()");
+        return 1;
+    }
+    if (bind(socket_fd, (struct sockaddr *)&socket_sin,
+      sizeof(struct sockaddr)) < 0) {
+        perror("bind()");
+        return -1;
+    } else {
+        printf("new bridge: usb_port[%u] <--> udp_port[%u]\n",
+          usb_port, udp_port);
+    }
 
-  ev = event_new(base, sock_fd, EV_READ | EV_PERSIST, &read_cb, st);
-  event_add(ev, NULL);
+    ev = event_new(base, socket_fd, EV_READ | EV_PERSIST, &udp_callback, s);
+    event_add(ev, NULL);
 }
 
 
-int extract_packet(char *buf, int len)
+int usb_extract(char *usb_rxbuf, int usb_rxlen)
 {
-  char *tmp=buf;
-  int i;
-  //int magic = ntohl(STRM_MAGIC);
-  int magic = STRM_MAGIC;
-  int canal;
-  int buflen;
-  char *out;
-  int                 size = sizeof(struct sockaddr);
+    int i;
+    int usb_preamble = FT_STREAM_PREAMBLE;
+    int usb_port;
 
-  if(len < 12)
+    char *udp_txbuf;
+    int udp_txlen;
+    int size = sizeof(struct sockaddr);
+
+    if(usb_rxlen < FT_STREAM_HEADER_SIZE)
+        return 0;
+    if(usb_rxbuf == NULL)
+        return 0;
+    for(i=0; i<usb_rxlen-4; i++) {
+        if(!memcmp(usb_rxbuf + i, &usb_preamble, 4)) {
+#ifdef DEBUG
+            printf("usb_preamble found at %d\n", i);
+#endif
+	          break;
+	      }
+    }
+    if(i == usb_rxlen - 4) {
+#ifdef DEBUG
+        printf("usb_preamble not found");
+#endif
+        return 0;
+    }
+    /* get rid of whatever before usb_preamble */
+    if(i != 0) {
+#ifdef DEBUG
+        printf("usb_preamble found at %d\n", i);
+#endif
+        return i;
+    }
+    /* get port */
+    memcpy(&usb_port, usb_rxbuf + 4, 4);
+    /* get length */
+    memcpy(&udp_txlen, usb_rxbuf + 8, 4);
+
+    /* get data and send over udp */
+    udp_txbuf = usb_rxbuf + FT_STREAM_HEADER_SIZE;
+    if(udp_txlen + FT_STREAM_HEADER_SIZE <= usb_rxlen)
+    {
+        if(usb_port < FT_STREAM_PORTS) {
+	          sendto(ft_streams[usb_port].fd, udp_txbuf, udp_txlen, 0,
+              (struct sockaddr *)&ft_streams[usb_port].client_addr, size);
+	      }
+        return udp_txlen + FT_STREAM_HEADER_SIZE;
+    }
+
     return 0;
-  if(buf == NULL)
-    return 0;
-  for(i = 0; i < len - 4; i++)
-    {
-
-      if(!memcmp(tmp + i, &magic, 4))
-	{
-	  printf("found magic at %d\n", i);
-	  break;
-	}
-    }
-
-  if(i == len -4)
-    {
-      printf("didn't find magic");
-	return 0;
-    }
-  if(i != 0) /*get rid of whatever before magic*/
-    {
-      printf("found magic at pos %d\n", i);
-      return i;
-    }
-  tmp +=4;
-  memcpy(&canal, tmp, 4);
-  //canal=ntohl(canal);
-  tmp+=4;
-  memcpy(&buflen, tmp, 4);
-  //buflen = ntohl(buflen);
-  tmp+=4;
-  printf("buflen and canal: %d %d\n", buflen, canal);
-  if(buflen + 12 <= len)
-    {
-      printf("Extracted %d bytes for canal %d\n", buflen, canal );
-      if(canal < 256)
-	{
-	  sendto(streams[canal].fd, tmp, buflen, 0, (struct sockaddr *)&streams[canal].client_addr, size);
-	}
-      for(i = 0; i < buflen; i++)
-	printf("%02x ", tmp[i]);
-      printf("\n");
-      return buflen + 12;
-    }
-  return 0;
 }
 
-void ft_rd_cb(PVOID pvCallbackContext, E_FT_NOTIFICATION_CALLBACK_TYPE eCallbackType, PVOID pvCallbackInfo)
+void usb_callback(PVOID pvCallbackContext,
+  E_FT_NOTIFICATION_CALLBACK_TYPE eCallbackType, PVOID pvCallbackInfo)
 {
+    unsigned char *usb_rxbuf;
+    int usb_rxlen;
 
-  unsigned char *buf;
-  int ret;
-  int i;
-  static unsigned char *fifo=NULL;
-  unsigned char *tmp, *tmp2;
-  static unsigned int len = 0;
-  int magic = STRM_MAGIC;
-  int buflen;
-  int canal;
+    static unsigned char *usb_cbfifo=NULL;
+    unsigned char *usb_cbbuf;
+    static unsigned int usb_cblen = 0;
 
-  FT_NOTIFICATION_CALLBACK_INFO_DATA *data = pvCallbackInfo;
-  printf("CALLED %d %d %02x\n", eCallbackType, data->ulRecvNotificationLength, data->ucEndpointNo);
+    FT_NOTIFICATION_CALLBACK_INFO_DATA *data = pvCallbackInfo;
+    usb_rxbuf = malloc(data->ulRecvNotificationLength);
+    usb_rxlen = FT601_Read(ft_handle, usb_rxbuf, data->ulRecvNotificationLength);
 
-  buf = malloc( data->ulRecvNotificationLength);
-  ret = FT601_Read(ftHandle, buf, data->ulRecvNotificationLength);
-  printf("ret: %d\n", ret);
-  for(i = 0; i < ret; i++)
-    printf("%02x ", buf[i]);
-      printf("\n");
+ #ifdef DEBUG
+    int i;
+    printf("usb_rx: ", usb_rxlen);
+    for(i = 0; i < usb_rxlen; i++)
+        printf("%02x", usb_rxbuf[i]);
+    printf("\n");
+#endif
 
-  tmp = malloc(len + ret);
-  memcpy(tmp, fifo, len);
-  memcpy(tmp + len, buf, ret);
-  len += ret;
-  free(buf);
-  free(fifo);
-  fifo = tmp;
-  printf("New LEN = %d\n", len);
-  do {
-    printf("current fifo:\n");
-    for(i = 0; i < len; i++)
-    printf("%02x ", fifo[i]);
-      printf("\n\n");
+    usb_cbbuf = malloc(usb_cblen + usb_rxlen);
+    memcpy(usb_cbbuf, usb_cbfifo, usb_cblen);
+    memcpy(usb_cbbuf + usb_cblen, usb_rxbuf, usb_rxlen);
+    usb_cblen += usb_rxlen;
+    free(usb_rxbuf);
+    free(usb_cbfifo);
+    usb_cbfifo = usb_cbbuf;
 
-    ret = extract_packet(fifo, len);
-    printf("extracted %d\n", ret);
-    if(ret == len)
-      {
-	free(fifo);
-	fifo = NULL;
-	len = 0;
-      }
-    else if(ret)
-      {
-	tmp = malloc(len - ret);
-	memcpy(tmp, fifo + ret, len - ret);
-	len = len - ret;
-	free(fifo);
-	fifo = tmp;
-      }
-  }while(ret);
-
+    do {
+        usb_rxlen = usb_extract(usb_cbfifo, usb_cblen);
+        if(usb_rxlen == usb_cblen) {
+	         free(usb_cbfifo);
+	         usb_cbfifo = NULL;
+	         usb_cblen = 0;
+        } else if (usb_rxlen) {
+	         usb_cbbuf = malloc(usb_cblen - usb_rxlen);
+	         memcpy(usb_cbbuf, usb_cbfifo + usb_rxlen, usb_cblen - usb_rxlen);
+           usb_cblen = usb_cblen - usb_rxlen;
+           free(usb_cbfifo);
+           usb_cbfifo = usb_cbbuf;
+        }
+    } while (usb_rxlen);
 }
-
-
-
 
 int main(int argc, char **argv)
 {
-  struct event_base *base;
-
+    struct event_base *base;
 
 #ifdef WIN32
-  WSADATA wsa_data;
-  WSAStartup(0x0201, &wsa_data);
+    WSADATA wsa_data;
+    WSAStartup(0x0201, &wsa_data);
 #endif
 
-  base = event_base_new();
-  if (!base) {
-    fprintf(stderr, "Could not initialize libevent!\n");
-    return 1;
-  }
+    base = event_base_new();
+    if (!base) {
+        return 1;
+    }
 
-  FT601_Open(&ftHandle);
-  FT_SetNotificationCallback(ftHandle, &ft_rd_cb, NULL);
+    FT601_Open(&ft_handle);
+    FT_SetNotificationCallback(ft_handle, &usb_callback, NULL);
 
-  new_canal(base,0, 1234);
-  new_canal(base,1, 2345);
+    new_usb_udp_bridge(base, 0, 1234);
+    new_usb_udp_bridge(base, 1, 2345);
 
-  event_base_dispatch(base);
+    event_base_dispatch(base);
+    event_base_free(base);
 
-  event_base_free(base);
-
-  printf("done\n");
-  return 0;
+    return 0;
 }
