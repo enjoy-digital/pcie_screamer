8136e6ee65a1b360a70b70ff85f9108cb9d57d8d
Florent Kermarrec <florent@enjoy-digital.fr>
2016-10-20 11:11:57 +0200

    gateware/tlp: fix TLPReceiver for even dwords count

--- gateware/tlp.py
+++ gateware/tlp.py
@@ -1,7 +1,10 @@
 from litex.gen import *
 
 from litex.soc.interconnect import stream
-from litex.soc.interconnect.stream import StrideConverter
+from litex.soc.interconnect.stream import Converter, StrideConverter
+
+import sys
+sys.path.append("../")
 
 from litepcie.common import phy_layout as tlp_description
 from gateware.usb import user_description as usb_description
@@ -80,17 +83,24 @@ class TLPReceiver(Module):
 
         # # #
 
-        converter = StrideConverter(tlp_description(32),
-                                    tlp_description(64))
+        converter = Converter(32, 64, report_valid_token_count=True)
         self.submodules += converter
 
         self.comb += [
             converter.sink.valid.eq(self.sink.valid),
             converter.sink.last.eq(self.sink.last),
             self.sink.ready.eq(converter.sink.ready),
-            converter.sink.dat.eq(self.sink.data),
-            converter.sink.be.eq(0xf),
-            converter.source.connect(source)
+            converter.sink.data.eq(self.sink.data),
+
+            source.valid.eq(converter.source.valid),
+            source.last.eq(converter.source.last),
+            If(converter.source.valid_token_count == 1,
+                source.be.eq(0x0f)
+            ).Else(
+                source.be.eq(0xff)
+            ),
+            source.dat.eq(converter.source.data),
+            converter.source.ready.eq(source.ready)
         ]
 
 
@@ -103,3 +113,28 @@ class TLP(Module):
             sender.source.connect(usb_port.sink),
             usb_port.source.connect(receiver.sink)
         ]
+
+
+if __name__ == '__main__':
+        receiver = TLPReceiver()
+
+        def generator(dut):
+            yield dut.source.ready.eq(1)
+            yield
+            yield dut.sink.valid.eq(1)
+            yield dut.sink.data.eq(0x01234567)
+            yield
+            yield dut.sink.valid.eq(1)
+            yield dut.sink.data.eq(0x89abcdef)
+            yield
+            yield dut.sink.valid.eq(1)
+            yield dut.sink.last.eq(1)
+            yield dut.sink.data.eq(0x11112222)
+            yield
+            yield dut.sink.valid.eq(0)
+            yield dut.sink.last.eq(0)
+            yield dut.sink.data.eq(0)
+            for i in range(16):
+                yield
+
+        run_simulation(receiver, generator(receiver), vcd_name="sim.vcd")
