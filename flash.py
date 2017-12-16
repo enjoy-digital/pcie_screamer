3a7e0b3507a7e503e141f9283b0eca7711d99267
Pierre-Olivier Vauboin <po@lambdaconcept.com>
2017-12-16 11:06:27 +0100

    program spi flash using openocd

---
+++ flash.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from litex.build.openocd import OpenOCD
+
+prog = OpenOCD("openocd.cfg",
+        flash_proxy_basename="bscan_spi_xc7a50t.bit")
+prog.set_flash_proxy_dir(".")
+prog.flash(0x0, "build/gateware/top.bin")
