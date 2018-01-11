8fd5031f30a4f7d1c1937b6078169a1ec2754d9f
Florent Kermarrec <florent@enjoy-digital.fr>
2018-01-11 17:38:01 +0100

    test/test_sdram: update

--- test/test_sdram.py
+++ test/test_sdram.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-from litex.soc.tools.remote import RemoteClient
 
+import sys
 import time
 
-# DDR3 init and test to find working bitslip/delay configuration
+from litex.soc.tools.remote import RemoteClient
+
+# DDR3 init and test for a7ddrphy design
+# use arty_ddr3 design with this script to
+# find working bitslip/delay configuration
 
 dfii_control_sel     = 0x01
 dfii_control_cke     = 0x02
@@ -17,60 +21,59 @@ dfii_command_ras    = 0x08
 dfii_command_wrdata = 0x10
 dfii_command_rddata = 0x20
 
-wb = RemoteClient()
+wb = RemoteClient(debug=False)
 wb.open()
-regs = wb.regs
 
 # # #
 
-regs.sdram_dfii_control.write(0)
+wb.regs.sdram_dfii_control.write(0)
 
 # release reset
-regs.sdram_dfii_pi0_address.write(0x0)
-regs.sdram_dfii_pi0_baddress.write(0)
-regs.sdram_dfii_control.write(dfii_control_odt|dfii_control_reset_n)
+wb.regs.sdram_dfii_pi0_address.write(0x0)
+wb.regs.sdram_dfii_pi0_baddress.write(0)
+wb.regs.sdram_dfii_control.write(dfii_control_odt|dfii_control_reset_n)
 time.sleep(0.1)
 
 # bring cke high
-regs.sdram_dfii_pi0_address.write(0x0)
-regs.sdram_dfii_pi0_baddress.write(0)
-regs.sdram_dfii_control.write(dfii_control_cke|dfii_control_odt|dfii_control_reset_n)
+wb.regs.sdram_dfii_pi0_address.write(0x0)
+wb.regs.sdram_dfii_pi0_baddress.write(0)
+wb.regs.sdram_dfii_control.write(dfii_control_cke|dfii_control_odt|dfii_control_reset_n)
 time.sleep(0.1)
 
 # load mode register 2
-regs.sdram_dfii_pi0_address.write(0x408)
-regs.sdram_dfii_pi0_baddress.write(2)
-regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
-regs.sdram_dfii_pi0_command_issue.write(1)
+wb.regs.sdram_dfii_pi0_address.write(0x408)
+wb.regs.sdram_dfii_pi0_baddress.write(2)
+wb.regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
+wb.regs.sdram_dfii_pi0_command_issue.write(1)
 
 # load mode register 3
-regs.sdram_dfii_pi0_address.write(0x0)
-regs.sdram_dfii_pi0_baddress.write(3)
-regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
-regs.sdram_dfii_pi0_command_issue.write(1)
+wb.regs.sdram_dfii_pi0_address.write(0x0)
+wb.regs.sdram_dfii_pi0_baddress.write(3)
+wb.regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
+wb.regs.sdram_dfii_pi0_command_issue.write(1)
 
 # load mode register 1
-regs.sdram_dfii_pi0_address.write(0x6);
-regs.sdram_dfii_pi0_baddress.write(1);
-regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
-regs.sdram_dfii_pi0_command_issue.write(1)
+wb.regs.sdram_dfii_pi0_address.write(0x6);
+wb.regs.sdram_dfii_pi0_baddress.write(1);
+wb.regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
+wb.regs.sdram_dfii_pi0_command_issue.write(1)
 
 # load mode register 0, cl=7, bl=8
-regs.sdram_dfii_pi0_address.write(0x930);
-regs.sdram_dfii_pi0_baddress.write(0);
-regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
-regs.sdram_dfii_pi0_command_issue.write(1)
+wb.regs.sdram_dfii_pi0_address.write(0x930);
+wb.regs.sdram_dfii_pi0_baddress.write(0);
+wb.regs.sdram_dfii_pi0_command.write(dfii_command_ras|dfii_command_cas|dfii_command_we|dfii_command_cs)
+wb.regs.sdram_dfii_pi0_command_issue.write(1)
 time.sleep(0.1)
 
 # zq calibration
-regs.sdram_dfii_pi0_address.write(0x400);
-regs.sdram_dfii_pi0_baddress.write(0);
-regs.sdram_dfii_pi0_command.write(dfii_command_we|dfii_command_cs)
-regs.sdram_dfii_pi0_command_issue.write(1)
+wb.regs.sdram_dfii_pi0_address.write(0x400);
+wb.regs.sdram_dfii_pi0_baddress.write(0);
+wb.regs.sdram_dfii_pi0_command.write(dfii_command_we|dfii_command_cs)
+wb.regs.sdram_dfii_pi0_command_issue.write(1)
 time.sleep(0.1)
 
 # hardware control
-regs.sdram_dfii_control.write(dfii_control_sel)
+wb.regs.sdram_dfii_control.write(dfii_control_sel)
 
 def seed_to_data(seed, random=True):
     if random:
@@ -82,37 +85,46 @@ def write_pattern(length):
     for i in range(length):
         wb.write(wb.mems.main_ram.base + 4*i, seed_to_data(i))
 
-def check_pattern(length):
+def check_pattern(length, debug=False):
     errors = 0
     for i in range(length):
         error = 0
         if wb.read(wb.mems.main_ram.base + 4*i) != seed_to_data(i):
             error = 1
-            print("{}: 0x{:08x}, 0x{:08x} KO".format(i, wb.read(wb.mems.main_ram.base + 4*i), seed_to_data(i)))
+            if debug:
+                print("{}: 0x{:08x}, 0x{:08x} KO".format(i, wb.read(wb.mems.main_ram.base + 4*i), seed_to_data(i)))
         else:
-            print("{}: 0x{:08x}, 0x{:08x} OK".format(i, wb.read(wb.mems.main_ram.base + 4*i), seed_to_data(i)))
+            if debug:
+                print("{}: 0x{:08x}, 0x{:08x} OK".format(i, wb.read(wb.mems.main_ram.base + 4*i), seed_to_data(i)))
         errors += error
     return errors
 
 # find working bitslips and delays
-for bitslip in range(4):
-    for delay in range(32):
-        for k in range(2):
-            regs.ddrphy_dly_sel.write(1<<k)
-            regs.ddrphy_rdly_dq_rst.write(1)
+nbitslips = 4
+ndelays = 32
+nmodules = 2
+nwords = 16
+
+for bitslip in range(nbitslips):
+    print("bitslip {:d}: |".format(bitslip), end="")
+    for delay in range(ndelays):
+        for module in range(nmodules):
+            wb.regs.ddrphy_dly_sel.write(1<<module)
+            wb.regs.ddrphy_rdly_dq_rst.write(1)
             for i in range(bitslip):
                 # 7-series SERDES in DDR mode needs 3 pulses for 1 bitslip
                 for j in range(3):
-                    regs.ddrphy_rdly_dq_bitslip.write(1)
+                    wb.regs.ddrphy_rdly_dq_bitslip.write(1)
             for i in range(delay):
-                regs.ddrphy_rdly_dq_inc.write(1)
-        print("bitslip={}, delay={}".format(bitslip, delay))
-        write_pattern(32)
-        if check_pattern(32) == 0:
-            print("bitslip={}, delay={}".format(bitslip, delay))
-            write_pattern(1024)
-            print("errors : %d" %check_pattern(1024))
-            exit()
+                wb.regs.ddrphy_rdly_dq_inc.write(1)
+        write_pattern(nwords)
+        errors = check_pattern(nwords)
+        if errors:
+            print("..|", end="")
+        else:
+            print("{:02d}|".format(delay), end="")
+        sys.stdout.flush()
+    print("")
 
 # # #
 
