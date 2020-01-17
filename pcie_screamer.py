f0d5954384c491e3c8ac4416107c2e4e80aee122
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-17 13:54:59 +0100

    pcie_screamer: simplify CRG since we no longer have the DDR3

--- pcie_screamer.py
+++ pcie_screamer.py
@@ -30,23 +30,16 @@ from litescope import LiteScopeAnalyzer
 class _CRG(Module):
     def __init__(self, platform, sys_clk_freq):
         self.clock_domains.cd_sys       = ClockDomain()
-        self.clock_domains.cd_sys4x     = ClockDomain(reset_less=True)
-        self.clock_domains.cd_sys4x_dqs = ClockDomain(reset_less=True)
-        self.clock_domains.cd_clk200    = ClockDomain()
         self.clock_domains.cd_usb       = ClockDomain()
 
         # # #
 
+        # sys
         self.submodules.pll = pll = S7PLL(speedgrade=-1)
         pll.register_clkin(platform.request("clk100"), 100e6)
         pll.create_clkout(self.cd_sys,       sys_clk_freq)
-        pll.create_clkout(self.cd_sys4x,     4*sys_clk_freq)
-        pll.create_clkout(self.cd_sys4x_dqs, 4*sys_clk_freq, phase=90)
-        pll.create_clkout(self.cd_clk200,    200e6)
 
-        self.submodules.idelayctrl = S7IDELAYCTRL(self.cd_clk200)
-
-        # usb clock domain (100MHz from usb)
+        # usb 100MHz
         self.comb += self.cd_usb.clk.eq(platform.request("usb_fifo_clock"))
         self.specials += AsyncResetSynchronizer(self.cd_usb, ResetSignal("pcie"))
 
