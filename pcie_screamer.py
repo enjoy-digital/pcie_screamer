4debf92c747ce19e7f56025ed9e140e6201d3581
Florent Kermarrec <florent@enjoy-digital.fr>
2020-01-26 15:26:57 +0100

    pcie_screamer/flash: fix filename

--- pcie_screamer.py
+++ pcie_screamer.py
@@ -154,7 +154,7 @@ def main():
         prog = OpenOCD("openocd/openocd.cfg",
             flash_proxy_basename="openocd/bscan_spi_xc7a35t.bit")
         prog.set_flash_proxy_dir(".")
-        prog.flash(0, "build/gateware/top.bit")
+        prog.flash(0, "build/gateware/top.bin")
 
 if __name__ == "__main__":
     main()
